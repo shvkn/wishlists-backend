af442cc619fc15c2931476f0c8a93f8ba73da1a0
Anatoly Shuvakhin <shuvakhin@yandex.ru>
2023-10-27 19:28:31 +0700

    feat(swagger): Подключить swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +14,20 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Kupipodariday API')
+    .setDescription('API сервиса вишлистов')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('users')
+    .addTag('wishes')
+    .addTag('offers')
+    .addTag('wishlists')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 bootstrap();
